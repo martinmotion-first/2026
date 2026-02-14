7c6fae1f17c9b3897152e45f376270b936195dac
martinmotion-first <154715309+martinmotion-first@users.noreply.github.com>
2026-02-14 10:37:35 -0600

    IDs set (temporarily at least depending)

--- 2026MM6237/src/main/java/frc/robot/Ports.java
+++ 2026MM6237/src/main/java/frc/robot/Ports.java
@@ -8,38 +8,39 @@ public final class Ports {
     public static final CANBus kCANivoreCANBus = new CANBus("main");
 
     // Talon FX IDs
-    public static final int kIntakePivot = 10;
-    public static final int kIntakeRollers = 11;
-    public static final int kFloor = 12;
-    public static final int kFeeder = 13;
-    public static final int kShooterLeft = 14;
-    public static final int kShooterMiddle = 15;
-    public static final int kShooterRight = 16;
-    public static final int kHanger = 18;
+    public static final int kIntakePivot = 12;
+    public static final int kFloor = 10;
+    public static final int kFeeder = 14;
+    public static final int kIntakeRollers = 99;//11;
+    public static final int kHanger = 99;//18;
+
+    public static final int kShooterLeft = 11;
+    public static final int kShooterMiddle = 2;
+    public static final int kShooterRight = 5;
 
     // ======================== SWERVE DRIVE MOTOR IDs ========================
     // Front Left Module
-    public static final int kFrontLeftDriveMotor = 8;
+    public static final int kFrontLeftDriveMotor = 13;
     public static final int kFrontLeftSteerMotor = 7;
-    public static final int kFrontLeftEncoder = 15;
+    public static final int kFrontLeftEncoder = 16;
 
     // Front Right Module
-    public static final int kFrontRightDriveMotor = 10;
-    public static final int kFrontRightSteerMotor = 12;
-    public static final int kFrontRightEncoder = 17;
+    public static final int kFrontRightDriveMotor = 3;
+    public static final int kFrontRightSteerMotor = 8;
+    public static final int kFrontRightEncoder = 19;
 
     // Back Left Module
-    public static final int kBackLeftDriveMotor = 9;
-    public static final int kBackLeftSteerMotor = 11;
-    public static final int kBackLeftEncoder = 18;
+    public static final int kBackLeftDriveMotor = 1;
+    public static final int kBackLeftSteerMotor = 9;
+    public static final int kBackLeftEncoder = 17;
 
     // Back Right Module
-    public static final int kBackRightDriveMotor = 14;
-    public static final int kBackRightSteerMotor = 13;
-    public static final int kBackRightEncoder = 16;
+    public static final int kBackRightDriveMotor = 4;
+    public static final int kBackRightSteerMotor = 6;
+    public static final int kBackRightEncoder = 18;
 
     // Gyro/Pigeon 2
-    public static final int kPigeon2Id = 19;
+    public static final int kPigeon2Id = 15;
 
     // PWM Ports
     public static final int kHoodLeftServo = 3;
